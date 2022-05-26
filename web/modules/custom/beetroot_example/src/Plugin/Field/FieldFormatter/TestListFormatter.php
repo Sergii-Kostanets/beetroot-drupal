4edbf838af0c2fbc62173ce5d297e1e62f9dcd42
Sergii-Kostanets <sergii.kostanets@ua.nestle.com>
2022-05-26 13:55:23 +0300

    test formstter

---
+++ web/modules/custom/beetroot_example/src/Plugin/Field/FieldFormatter/TestListFormatter.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\beetroot_example\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\Annotation\FieldFormatter;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+
+/**
+ * @FieldFormatter(
+ *   id = "test_list",
+ *   label = @Translation("Test List"),
+ *   field_types = {
+ *     "list_integer",
+ *     "list_float",
+ *     "list_string",
+ *   }
+ * )
+ */
+class TestListFormatter extends FormatterBase {
+
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    return ['#markup' => 'Test values for field: ' . $items->getFieldDefinition()->getName() . '.'];
+  }
+
+}
