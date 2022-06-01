0e83b1c5b8f59e4ffd0c33f1dfdc60ac88708e4d
Sergii-Kostanets <sergii.kostanets@ua.nestle.com>
2022-06-01 14:04:48 +0300

    cron

---
+++ web/modules/custom/beetroot_example/src/Plugin/QueueWorker/AdminNotifyWorker.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\beetroot_example\Plugin\QueueWorker;
+
+use Drupal\Core\Annotation\QueueWorker;
+use Drupal\Core\Queue\QueueWorkerBase;
+use Drupal\ds\Plugin\DsField\User\User;
+
+/**
+ * Defines 'admin_notify_worker' queue worker.
+ *
+ * @QueueWorker(
+ *   id = "admin_notify_worker",
+ *   title = @Translation("Admin Notify"),
+ *   cron = {"time" = 60}
+ * )
+ */
+class AdminNotifyWorker extends QueueWorkerBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processItem($id) {
+    $user = User::load($id);
+    // @todo Send email.
+  }
+
+}
