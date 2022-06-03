d8a6a1dd43da867db03ad7aea4f059acc593b5b9
Sergii-Kostanets <sergii.kostanets@ua.nestle.com>
2022-06-03 12:40:13 +0300

    _update

---
+++ web/modules/custom/beetroot_example/beetroot_example.post_update.php
@@ -0,0 +1,35 @@
+<?php
+
+use Drupal\block_content\Entity\BlockContent;
+
+/**
+ * Add new custom page.
+ */
+function beetroot_example_post_update_create_some_page(&$sandbox) {
+  \Drupal\node\Entity\Node::create([
+    'type' => 'page',
+    'title' => 'Some page',
+    'status' => 1,
+  ])->save();
+  \Drupal::messenger()->addMessage('Added Some page.');
+}
+
+/**
+ * Add adv block.
+ */
+function beetroot_example_post_update_add_new_block(&$sandbox) {
+  BlockContent::create([
+    "uuid" => "5971c447-798e-4847-b9e5-97b3bd828a98",
+    "type" => "basic",
+    "status" => "1",
+    "info" => "New Block",
+    "reusable" => "1",
+    "body" => [
+      [
+        "value" => "<p>Some new block</p>\r\n",
+        "summary" => "",
+        "format" => "basic_html",
+      ],
+    ],
+  ])->save();
+}
